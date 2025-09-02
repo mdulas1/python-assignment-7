3174869faee3ddd4e1073de03a7aa4958975e05c
mdulas1 <maawiyajibrintanimu126@gmail.com>
2025-09-02 08:45:28 +0100

    first commit

--- attendance.py
+++ attendance.py
@@ -20,23 +20,62 @@ attendance = {}
 
 def register_student(student_id, name):
     """Register a student in the system."""
-    pass
+    if student_id not in attendance:
+        attendance[student_id] = {
+            "name":name,
+            'present_days':[],
+            "absent_days":[]
+        }
+    else:
+        print(f"{student_id} already registered")
+
 
 def mark_present(student_ids):
     """Mark multiple students as present for today."""
     today = str(datetime.date.today())
     # implement logic
-    pass
+    for stu_id in student_ids:
+        if stu_id in attendance:
+            if today not in attendance[stu_id]["present_days"]:
+                attendance[stu_id]["present_days"].append(today)
+            if today in attendance[stu_id]["absent_days"]:
+                attendance[stu_id]["absent_days"].remove(today)
+            else:
+                print(f"student {stu_id} not found")
 
 def mark_absent(student_ids):
     """Mark multiple students as absent for today."""
     today = str(datetime.date.today())
     #implement logic
-    pass
+    for stu_id in student_ids:
+        if stu_id in attendance:
+            if today not in attendance[stu_id]["absent_days"]:
+                attendance[stu_id]["absent_days"].append(today)
+            if today in attendance[stu_id]["present_days"]:
+                attendance[stu_id]["absent_days"].remove(today)
+            else:
+                print(f"student {stu_id} not found")
+
 
 def get_report(**kwargs):
     """Generate attendance report with optional filters."""
     report = {}
     # implement logic
+    for stu_id,data in attendance.items():
+        if kwargs.get("only_present") and not data["present_days"]:
+            continue
+        if kwargs.get("only absent") and not data["absent_days"]:
+            continue
+        report[stu_id] = data
+
 
     return report
+register_student(1,"ummi")
+register_student(2,"godiya")
+register_student(3,"oklo")
+
+mark_present([1,3])
+mark_absent([2])
+print("report",get_report())
+print("only_present",get_report(only_present=True))
+print("only_absent",get_report(only_absent=True))
--- banking.py
+++ banking.py
@@ -18,19 +18,67 @@ accounts = {}
 
 def create_account(account_number, name, **kwargs):
     """Create an account with optional features like overdraft_limit."""
-    pass
+    if account_number in accounts:
+        return f"account {account_number} aready exist"
+    accounts[account_number] = {
+        "name":name,
+        "balance":0.0
+    }
+    #add any extra features (e.g , overdraft_limit)
+    for key, value in kwargs.items():
+        accounts[account_number][key] = value
+    return f"Account created for {name} with account number {account_number}"
 
-def deposit(account_number, amount):
+def deposit(account_number, *amounts):
     """Deposit money into account.
         return "Account not found!" (if account does not exists)
         return Deposited {amount} into {accounts name}'s account. if account exists
     """
-    pass
+    if account_number not in accounts:
+        return "Account not found"
+    total = sum(amounts)
+    accounts[account_number]["balance"] += total
+    return f"Doposited {total} into {accounts[account_number]["name"]}'s account"
+
 
 def withdraw(account_number, amount):
     """Withdraw money if balance is sufficient. else: insufficient funds"""
-    pass
+    if account_number not in accounts:
+        return "Account not found"
+    balance = accounts[account_number]["balance"]
+    overdraft = accounts[account_number].get("overdraft_limit",0)
 
+    if balance + overdraft >= amount:
+        accounts[account_number]["balance"] -= amount
+        return f"Withdrew {amount} from {accounts[account_number]["name"]} account"
+    else:
+        return "insufficient funds"
+    
 def transfer(from_acc, to_acc, amount):
     """Transfer money between accounts. if funds is sufficient"""
-    pass
+    if from_acc not in accounts:
+        return f"Sender account {from_acc} not found!"
+    if to_acc not in accounts:
+        return f"Receiver account {to_acc} not found!"
+    
+    balance = accounts[from_acc]["balance"]
+    overdraft = accounts[from_acc].get("overdraft_limit", 0)
+    
+    if balance + overdraft >= amount:
+        accounts[from_acc]["balance"] -= amount
+        accounts[to_acc]["balance"] += amount
+        return f"Transferred {amount} from {accounts[from_acc]['name']} to {accounts[to_acc]['name']}."
+    else:
+        return "Insufficient funds for transfer!"
+    
+print(create_account(10,"jibrin"))
+print(create_account(11,"tanko",overdraft_limit=200))
+
+print(deposit(10,400,1000))
+print(deposit(11,500))
+
+print(withdraw(10,700))
+print(withdraw(11,300))
+print(transfer(10,11,100))
+
+print(accounts)
